8e78a01e2a979c16777a1cd0389dbc0401c4abf9
HappyHappyJoyJoy <joyumali@gmail.com>
2016-07-21 17:34:44 -0700

    3 RGB Sliders and a button to a second screen.

--- RGB-Sliders/ViewController.swift
+++ RGB-Sliders/ViewController.swift
@@ -10,9 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var colorSquare: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // want background color to be grey when app first opens. (0.5 of RGB = grey not white)
+        updateBackgroundColor(view)
+        
+        // give the square view a one pixel black border
+        colorSquare.layer.borderColor = UIColor.blackColor().CGColor
+        colorSquare.layer.borderWidth = 1
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,5 +33,19 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func updateBackgroundColor(sender: AnyObject) {
+        let red = CGFloat(redSlider.value)
+        let green = CGFloat(greenSlider.value)
+        let blue = CGFloat(blueSlider.value)
+        
+        /* Code below is to update background color of entire screen.
+        view.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+        */
+        
+        // Code for updating color of just the colorSquare instead of the screen.
+        colorSquare.backgroundColor = UIColor(red: red, green: green, blue: blue, alpha: 1)
+    }
+    
+    
 }
 
